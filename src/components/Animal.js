8d9ca8a782b2272f82b2de17855109a809406876
April Zhang <aprilzh23@gmail.com>
2023-06-12 14:55:38 -0700

    make component for animal

---
+++ src/components/Animal.js
@@ -0,0 +1,14 @@
+import './Animal.css';
+// - name
+// - species
+// - photo
+const Animal = () => {
+
+    return (<section className='Animal'>
+        <h3>Animal name: Cookie</h3>
+        <p>Species: Cat</p>
+        <img src="https://i.etsystatic.com/8213963/r/il/61131e/3746215281/il_fullxfull.3746215281_gdzt.jpg"></img>
+    </section>);
+};
+
+export default Animal;
