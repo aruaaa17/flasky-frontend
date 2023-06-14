50f02e2c1ad50a58525c0a06d981ef5789c5b5a2
April Zhang <aprilzh23@gmail.com>
2023-06-14 10:50:23 -0700

    add props

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,12 +1,17 @@
 import './Animal.css';
+
+// The responsibility of this component is to be a reusable UI element
 // - name
 // - species
 // - photo
-const Animal = () => {
+// - age
+const Animal = (props) => {
 
-    return (<section className='Animal'>
-        <h3>Animal name: Cookie</h3>
-        <p>Species: Cat</p>
+
+    return (
+    <section className='Animal'>
+        <h3>Animal name: {props.name} uwu</h3>
+        <p>Species: {props.species}</p>
         <img src="https://i.etsystatic.com/8213963/r/il/61131e/3746215281/il_fullxfull.3746215281_gdzt.jpg"></img>
     </section>);
 };
--- src/components/AnimalLis.js
+++ src/components/AnimalLis.js
@@ -2,17 +2,41 @@ import './AnimalList.css'
 import Animal from './Animal';
 
 const AnimalList = () => {
+    const firstAnimal = {
+        name: "Cookie",
+        species: "Cutie Cat"
+    };
+    const secondAnimal = {
+        name: "Norman",
+        species: "puppy"
+    };
+
+    const listOfAnimals = [
+        firstAnimal, 
+        secondAnimal,
+        {
+            name: "Kiki",
+            species: "Emmm?"
+        }
+    ];
+
+    const renderedAnimals = listOfAnimals.map(creature => (
+        <li>
+            <Animal
+                name={creature.name}
+                species={creature.species}
+            />
+        </li>
+    ));
+
+
     return (
         <section className="AnimalList">
             <h2>Animal List</h2>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
             <ul className="AnimalList__list">
-                <li><Animal></Animal></li>
+                <li>
+                    {renderedAnimals}
+                </li>
                 <li><Animal></Animal></li>
                 <li><Animal></Animal></li>
                 <li><Animal></Animal></li>
