822f1559b77777cd6b07ddd1ff6abcf2f1046b6b
April Zhang <aprilzh23@gmail.com>
2023-06-14 11:15:41 -0700

    refactor the AnimalList.js

--- src/components/AnimalLis.js
+++ src/components/AnimalLis.js
@@ -20,23 +20,46 @@ const AnimalList = () => {
         }
     ];
 
-    const renderedAnimals = listOfAnimals.map(creature => (
-        <li>
-            <Animal
-                name={creature.name}
-                species={creature.species}
-            />
-        </li>
-    ));
+    // for loop is a statement and cannot be wrote directly in the 'return' below
+    // const renderedAnimals = [];
+    // for (const creature of listOfAnimals) {
+    //     renderedAnimals.push(
+    //     <li>
+    //         <Animal
+    //             name={creature.name}
+    //             species={creature.species}
+    //         />
+    //     </li>
+    //     );
+    // }
+
+    // const renderedAnimals = listOfAnimals.map(creature => (
+    //     <li>
+    //         <Animal
+    //             name={creature.name}
+    //             species={creature.species}
+    //         />
+    //     </li>
+    // ));
 
 
     return (
         <section className="AnimalList">
             <h2>Animal List</h2>
             <ul className="AnimalList__list">
-                <li>
+                {/* <li>
                     {renderedAnimals}
-                </li>
+                </li> */}
+                {
+                    listOfAnimals.map(creature => (
+                        <li>
+                            <Animal
+                                name={creature.name}
+                                species={creature.species}
+                            />
+                        </li>
+                    ))
+                };
                 <li><Animal></Animal></li>
                 <li><Animal></Animal></li>
                 <li><Animal></Animal></li>
