d2b12fc6446db431ccc88ccc35bc8bfac49b6b97
April Zhang <aprilzh23@gmail.com>
2023-06-15 11:38:53 -0700

    add useState for bookmark@button to change bgcolor

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,14 +1,34 @@
 import './Animal.css';
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
+
 // The responsibility of this component is to be a reusable UI element that displays an Animal's
 const Animal = (props) => {
+    
+    const [isBookmarked, setIsBookmarked] = useState(false);
+
+    const altText = `Photo of ${props.name}`
+
+    const toggleBookMark = () => {
+        setIsBookmarked(!isBookmarked);
+        // console.log('We can click on this button!')
+    }
+
+    // If the animal is bookmarked, the CSS class name should be .bookmarked, If the animal is not bookmarked, there should be no CSS class
+    let animalBookmarkStyle = '';
+    if (isBookmarked) {
+        animalBookmarkStyle = 'bookmarked';
+    }
 
     return (
-    <section className="Animal">
+    <section className={animalBookmarkStyle}>
         <h3>Name: { props.name } (cute!)</h3>
         <p>Species: { props.species }</p>
-        { props.photo ? <img src={ props.photo } alt="Photo of Willow"></img> : "[No Photo]" }
+        { props.photo ? <img src={ props.photo } alt={altText}></img> : "[No Photo]" }
+        <button onClick={toggleBookMark}>Bookmark</button>
+        <p>Is bookmarked? {isBookmarked ? 'Yes, it is!' : 'No, it is not!'} </p>
+
     </section>);
 };
 
